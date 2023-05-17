1f8e788989cfd1c8ae3ceba8485083acf78ac914
Виктор Чернюк <vetrolego@gmail.com>
2023-05-17 18:54:56 +0300

    added relative path for exercise's photo

--- api/exercise/serializers.py
+++ api/exercise/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
 from exercise.models import Exercise
+from os import sep
+from functools import reduce
+
+def get_relative_path(obj):
+    separated_path = obj.photo.file.name.split(sep)
+    relative_path = '/' + '/'.join(separated_path[separated_path.index('media'):])
+    return relative_path
 
 class ExerciseSerializer(serializers.ModelSerializer):
+    photo = serializers.SerializerMethodField()
+
+    def get_photo(self, obj):
+        return get_relative_path(obj)
+    
+    
     class Meta:
         model = Exercise
         fields = '__all__'
