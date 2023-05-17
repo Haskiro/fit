ce1925ec29251080e3cc8eb4fa9f53891b49887e
Павел <58857483+Haskiro@users.noreply.github.com>
2023-05-17 16:15:10 +0300

    endpoint for set exercise and program relation, relative instead of absolute

--- api/program/views.py
+++ api/program/views.py
@@ -3,6 +3,9 @@ from program.serializers import ProgramSerializer
 from program.models import Program
 from rest_framework.filters import SearchFilter
 from rest_framework import permissions
+from rest_framework.decorators import action
+from exercise.models import Exercise
+from rest_framework.response import Response
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
@@ -18,3 +21,17 @@ class ProgramViewSet(ModelViewSet):
     search_fields = ['title']
     filter_backends = (SearchFilter,)
     permission_classes = [IsAdminOrReadOnly]
+
+    @action(methods=['POST'], detail=True)
+    def add_exercise_to_program(self, request, pk=None):
+        data = request.data
+        programs = Program.objects.get(pk=pk)
+        for program_id in data:
+            try:
+                exercise = Exercise.objects.get(id=program_id)
+                programs.tracks.add(exercise)
+            except Exercise.DoesNotExist:
+                return Response({'error': 'exercise with id=' + program_id + 'does not exist'})
+        programs.save()
+
+        return Response({'message': "exercise added"}, 201)
