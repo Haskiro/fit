e1b029f834be11c356c47c7ff2bcf7868956d305
Bekmuratov-MBF <bekmuratovdilmurat18@gmail.com>
2023-05-24 02:02:12 +0300

    added serializer and viwe for user

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from rest_framework.exceptions import ValidationError, ParseError, NotFound, AuthenticationFailed
+from rest_framework.viewsets import ModelViewSet
+from authentication.serializers import UserSerializer
+from authentication.models import User
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated
+from django.urls import reverse, reverse_lazy
+
+
+
+# Create your views here.
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @action(methods=['POST'], detail=False, url_path='register')
+    def register(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        user = User.objects.get(email=request.data['email'])
+
+        refresh = RefreshToken.for_user(user)
+        response = Response()
+        response.data = {'access': str(refresh.access_token)}
+        return response
+
+    @action(methods=['POST'], detail=False, url_path='login')
+    def login(self, request):
+        if 'email' not in request.data:
+            raise ValidationError({'error': 'email must not be empty'})
+        if 'password' not in request.data:
+            raise ValidationError({'error': 'password must not be empty'})
+
+        try:
+            user = User.objects.get(email=request.data['email'])
+        except User.DoesNotExist:
+            raise NotFound({'error': 'user with this email was not found'})
+
+        if not user.check_password(request.data['password']):
+            raise AuthenticationFailed({'error': 'incorrect password'})
+
+        refresh = RefreshToken.for_user(user)
+        response = Response()
+        response.data = {'access': str(refresh.access_token)}
+        return response
 
 # Create your views here.
