2048b223a52c64954591e3c265d699169de8b373
Виктор Чернюк <vetrolego@gmail.com>
2023-05-24 14:15:16 +0300

    added burger-menu in header

--- app/src/components/headerComponent/HeaderComponent.vue
+++ app/src/components/headerComponent/HeaderComponent.vue
@@ -8,6 +8,10 @@
 				</div>
 				<p class="logo-block__slogan">Надо подкачаться!</p>
 			</router-link>
+			<input id="menu-toggle" class="header__menu-toggle" type="checkbox" />
+			<label class="header__menu-btn" for="menu-toggle">
+				<span class="header__burger"></span>
+			</label>
 			<nav class="header__nav-block nav-block">
 				<router-link :to="{ name: 'PageInDevelopment' }" class="nav-block__link">Главная</router-link>
 				<router-link :to="{ name: 'PageInDevelopment' }" class="nav-block__link">О нас</router-link>
@@ -43,6 +47,60 @@ export default {};
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
+	}
+
+	&__menu-toggle {
+		display: none;
+
+		&:checked ~ .header__menu-btn > span {
+			transform: rotate(0);
+			background: rgba(0, 0, 0, 0);
+		}
+
+		&:checked ~ .header__menu-btn > span::before {
+			transform: rotate(45deg);
+			margin-top: 8px;
+		}
+
+		&:checked ~ .header__menu-btn > span::after {
+			transform: rotate(135deg);
+		}
+
+		&:checked ~ .nav-block {
+			visibility: visible;
+		}
+	}
+
+	&__menu-btn {
+		display: none;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+		z-index: 10;
+
+		@media (max-width: 1180px) {
+			display: flex;
+		}
+	}
+
+	&__burger,
+	&__burger::before,
+	&__burger::after {
+		display: block;
+		position: absolute;
+		width: 30px;
+		height: 2px;
+		background-color: #ffffff;
+	}
+
+	&__burger::before {
+		content: '';
+		margin-top: -8px;
+	}
+
+	&__burger::after {
+		content: '';
+		margin-top: 8px;
 	}
 }
 
@@ -95,6 +153,20 @@ export default {};
 	justify-content: space-between;
 	align-items: center;
 
+	@media (max-width: 1180px) {
+		margin: 0;
+		top: 0;
+		left: 0;
+		visibility: hidden;
+		position: absolute;
+		width: 100%;
+		height: 100vh;
+		background-color: #022637;
+		justify-content: center;
+		flex-direction: column;
+		z-index: 9;
+	}
+
 	&__link {
 		font-family: 'Roboto', sans-serif;
 		font-size: 18px;
@@ -102,29 +174,16 @@ export default {};
 		color: #fcfcfc;
 		text-transform: uppercase;
 		text-decoration: none;
+
+		@media (max-width: 1180px) {
+			margin-bottom: 18px;
+		}
 
 		&--btn {
 			padding: 16px 42px;
 			color: #000000;
 			background-color: #0fb9bc;
 		}
-	}
-}
-
-.sidebar-nav {
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-
-	&__link {
-		font-family: 'Roboto', sans-serif;
-		font-size: 18px;
-		font-weight: 400;
-		color: #fcfcfc;
-		text-transform: uppercase;
-		text-decoration: none;
-		margin-bottom: 20px;
 	}
 }
 </style>
